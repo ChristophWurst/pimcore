3b55d31df1e4c7b83a59782fce62a10a9df4a2c9
Christoph Wurst <wurst.christoph@gmail.com>
2015-12-05 19:57:29 +0100

    try to fix php-fpm

--- build/travis/setup-fpm.sh
+++ build/travis/setup-fpm.sh
@@ -4,9 +4,10 @@ echo "Setting up FPM ..."
 
 phpenv config-add build/travis/php.ini
 
-sudo cp -f build/travis/php-fpm.conf ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+cp -f build/travis/php-fpm.conf ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
-sudo cp -f build/travis/apache-fpm.conf /etc/apache2/sites-available/default
+#sudo cp -f build/travis/apache-fpm.conf /etc/apache2/sites-available/default
+cp -f build/travis/apache-fpm.conf build/travis-ci-apache
